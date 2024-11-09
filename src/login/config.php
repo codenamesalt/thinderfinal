a6f2d1d4f3a23378ff758b56942a75c6a0900dcd
nrthompson925 <nrthompson@bluevalleyk12.net>
2024-11-09 17:44:55 -0600

    php worky

--- src/login/config.php
+++ src/login/config.php
@@ -1,7 +1,7 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'final.thinder.com');
+define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'raspberry');
 define('DB_NAME', 'thinder');
