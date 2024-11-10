fa3dfc6081ffcddf8e31ff2683be40da5e9d4b82
nrthompson925 <nrthompson@bluevalleyk12.net>
2024-11-09 19:37:37 -0600

    test

--- src/login/tregister.php
+++ src/login/tregister.php
@@ -73,7 +73,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
          
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password, $param_city, $param_state, $param_phonenumber);
+            mysqli_stmt_bind_param($stmt, "sssss", $param_username, $param_password, $param_city, $param_state, $param_phonenumber);
             
             // Set parameters
             $param_username = $username;
