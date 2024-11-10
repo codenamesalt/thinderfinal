9249c96a6da2140a0c2391ed8fd70f16c93567f0
nrthompson925 <nrthompson@bluevalleyk12.net>
2024-11-10 07:38:05 -0600

    restoring welcoime

--- src/login/welcome.php
+++ src/login/welcome.php
@@ -14,28 +14,43 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <head>
     <meta charset="UTF-8">
     <title>Welcome</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="welcome.css">
+    <link rel="stylesheet" href="../navbar/navbar.css">
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
     <style>
         body{ font: 14px sans-serif; text-align: center; }
     </style>
 </head>
 <body>
+<header>
+    <div id="nav-placeholder"></div>
+    </header>
+
+    <script>
+    $(function(){
+      $("#nav-placeholder").load("../navbar/navbar.html");
+    });
+    </script>
     <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
     <p>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+        <a href="../swipe/swipe.php" class="btn">Start Swiping</a>
     </p>
     <h1>Registered Therapists</h1>
     <?php
-$host    = "localhost";
-$user    = "root";
-$pass    = "raspberry";
+$host = "localhost";
+$user = "root";
+$password = "raspberry";
 $db_name = "thinder";
+
 //create connection
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$connection = mysqli_connect($host, $user, $pass, $db_name);
+$connection = mysqli_connect($host, $user, $password, $db_name);
+
 //get results from database
-$result = mysqli_query($connection, "SELECT * FROM therapist");
+$result = mysqli_query($connection, "SELECT username, email, phonenumber, city, state FROM therapist");
+
 //showing property
 echo '<table class="data-table">
         <tr class="data-heading">';  //initialize table tag
@@ -43,6 +58,7 @@ while ($property = mysqli_fetch_field($result)) {
     echo '<td>' . htmlspecialchars($property->name) . '</td>';  //get field name for header
 }
 echo '</tr>'; //end tr tag
+
 //showing all data
 while ($row = mysqli_fetch_row($result)) {
     echo "<tr>";
