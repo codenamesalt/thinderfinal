75bdebcb33a5648d0126b55165734338ef327f6d
EAMalyshev <eamalyshev@bluevalleyk12.net>
2024-11-10 06:59:30 -0600

    jklsdgf

--- src/login/reset-password.php
+++ src/login/reset-password.php
@@ -76,6 +76,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <title>Reset Password</title>
     <link rel="stylesheet" href="../navbar/navbar.css">
+    <link rel="stylesheet" href="reset-password.css">
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
     <style>
         body{ font: 14px sans-serif; }
