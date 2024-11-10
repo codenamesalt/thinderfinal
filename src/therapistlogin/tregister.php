fb5fc4217011a204fa9f689e0d49041897cc94ef
nrthompson925 <nrthompson@bluevalleyk12.net>
2024-11-09 18:45:41 -0600

    fixed bugs

--- src/therapistlogin/tregister.php
+++ src/therapistlogin/tregister.php
@@ -73,7 +73,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO therapist (username, password) VALUES (?, ?)";
          
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
