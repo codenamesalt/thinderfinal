b28070f990287b8b53f7d0fcf33b1f934ef54bbc
nrthompson925 <nrthompson@bluevalleyk12.net>
2024-11-10 03:15:08 -0600

    various changes ;)

--- src/login/welcome.php
+++ src/login/welcome.php
@@ -36,6 +36,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
         <a href="../swipe/swipe.php" class="btn">Start Swiping</a>
+        <a href="../settings/susername.php" class="btn btn-warning">Settings</a>
     </p>
     <h1>Registered Therapists</h1>
     <?php
