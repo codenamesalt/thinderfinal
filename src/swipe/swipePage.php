a19ddf43537e244dcfe11327ccda598fe1d58e5b
EAMalyshev <eamalyshev@bluevalleyk12.net>
2024-11-10 07:53:15 -0600

    jkla;sdf

--- src/swipe/swipePage.php
+++ src/swipe/swipePage.php
@@ -33,10 +33,10 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </script>
     <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to Swipe.</h1>
     <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
-        <!-- <a href="../swipe/swipePage.php" class="btn">Start Swiping</a> -->
-        <a href="../settings/susername.php" class="btn btn-warning">Settings</a>
+        <a href="reset-password.php" class="btn-primary">Reset Your Password</a>
+        <a href="logout.php" class="btn-primary">Sign Out of Your Account</a>
+        <a href="../swipe/swipePage.php" class="btn-primary">Start Swiping</a>
+        <a href="../settings/susername.php" class="btn-primary">Settings</a>
     </p>
     <h1>Registered Therapists</h1>
     <?php
